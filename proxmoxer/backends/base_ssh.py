ba36a20ba159cba732438eae7014ea4f7ce8b6b8
John Hollowell <johnahollowell@gmail.com>
2020-12-20 02:37:51 +0000

    Fix SSH cross-service compatibility issue

--- proxmoxer/backends/base_ssh.py
+++ proxmoxer/backends/base_ssh.py
@@ -54,7 +54,9 @@ class ProxmoxBaseSSHSession(object):
             data['tmpfilename'] = tmp_filename
 
         translated_data = ' '.join(["-{0} {1}".format(k, v) for k, v in chain(data.items(), params.items())])
-        full_cmd = '{0}sh {1} --output-format json'.format(self.service, ' '.join(filter(None, (cmd, url, translated_data))))
+        # only PVE needs the format options, other services default to (or only have) JSON
+        format_options = "--output-format json" if self.service == "pve" else ""
+        full_cmd = '{0}sh {1} {2}'.format(self.service, ' '.join(filter(None, (cmd, url, translated_data))), format_options)
 
         stdout, stderr = self._exec(full_cmd)
         def match(s): return re.match(r'\d\d\d [a-zA-Z]', s)
